374a31af266ef990d36c6434a80b7bade44b2e58
40338732 <40338732@users.noreply.github.com>
2020-06-21 18:53:33 +0100

    First Section done BaseViewModel

---
+++ ViewModels/BaseViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Added as part of project
+using System.ComponentModel;
+
+namespace WpfApp4.ViewModels
+{
+    public class BaseViewModel:INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnChanged(string name)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
