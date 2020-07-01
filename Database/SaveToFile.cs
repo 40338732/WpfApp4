99c9785ca78a6ac78b43f81af24da3f9d63d80c1
40338732 <40338732@users.noreply.github.com>
2020-07-01 07:20:29 +0100

    Page 10

---
+++ Database/SaveToFile.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+using WpfApp4.Models;
+
+namespace WpfApp4.Database
+{
+   public  class SaveToFile
+    {
+        private string _csvFilePath;
+
+        public string ErrorCode { get; set; }
+
+        public SaveToFile()
+        {
+            _csvFilePath = "C:\\univ\\orders.txt";
+
+            ErrorCode = string.Empty;
+        }
+
+        public bool ToCsv( Order order)
+        {
+            try 
+            {
+                string csv = $"{order.Name},{order.Price.ToString()},{order.IsPaid.ToString()}";
+                csv = csv + Environment.NewLine;
+                File.AppendAllText(_csvFilePath, csv.ToString());
+                return true;
+            }
+            catch(Exception ex)
+            {
+                ErrorCode = ex.ToString();
+                return false;
+
+            }
+
+
+
+        }
+    }
+}
